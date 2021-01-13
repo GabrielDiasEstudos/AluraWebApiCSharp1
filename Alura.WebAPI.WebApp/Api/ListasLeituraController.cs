3e96ee060a42628109eb1f29ae90a6c39f3683df
Gabriel Dias Gonçalves <gabrieldiadeath90@gmail.com>
2021-01-12 21:50:51 -0300

    Add projeto Web API Alura C# parte 1

---
+++ Alura.WebAPI.WebApp/Api/ListasLeituraController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Alura.ListaLeitura.Modelos;
+using Alura.ListaLeitura.Persistencia;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Lista = Alura.ListaLeitura.Modelos.ListaLeitura;
+
+namespace Alura.WebAPI.WebApp.Api
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ListasLeituraController : ControllerBase
+    {
+        private readonly IRepository<Livro> _repo;
+
+        public ListasLeituraController(IRepository<Livro> repo)
+        {
+            _repo = repo;
+        }
+
+        private Lista CriarLista(TipoListaLeitura tipo)
+        {
+            return new Lista
+            {
+                Tipo = tipo.ParaString(),
+                Livros = _repo.All
+                    .Where(l => l.Lista == tipo)
+                    .Select(l => l.ToApi())
+                    .ToList()
+            };
+        }
+
+        [HttpGet]
+        public IActionResult TodasListas()
+        {
+            var paraLer = CriarLista(TipoListaLeitura.ParaLer);
+            var lendo = CriarLista(TipoListaLeitura.Lendo);
+            var lidos = CriarLista(TipoListaLeitura.Lidos);
+
+            var colecao = new List<Lista>();
+            
+            colecao.Add(paraLer);
+            colecao.Add(lendo);
+            colecao.Add(lidos);
+
+            return Ok(colecao);
+        }
+
+        [HttpGet("{tipo}")]
+        public IActionResult Recuperar(TipoListaLeitura tipo)
+        {
+            var lista = CriarLista(tipo);
+            return Ok(lista);
+        }
+    }
+}
